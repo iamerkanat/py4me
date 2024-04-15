3144b6ee56ab9e4b8e17002676df7dfc6a7b4e51
YERKANAT <93324789+iamerkanat@users.noreply.github.com>
2024-04-15 21:34:28 +0000

    still exhausted

--- myday.py
+++ myday.py
@@ -1,1 +1,3 @@
 fifteensApril = 'really sucks as yesterday and pre-yesterday, I am tired of kali linux wireless 802.11n'
+
+sixteensApril = "well it is 2:33AM and I am still solving problem of kali wireless network 802.11n"
