698bd23cb5ee965715061f8184dc0de15675104d
YERKANAT <93324789+iamerkanat@users.noreply.github.com>
2024-04-16 18:54:48 +0000

    day

--- myday.py
+++ myday.py
@@ -1,3 +1,45 @@
 fifteensApril = 'really sucks as yesterday and pre-yesterday, I am tired of kali linux wireless 802.11n'
 
 sixteensApril = "well it is 2:33AM and I am still solving problem of kali wireless network 802.11n"
+
+
+def life(name):
+  print(name)
+  sleep()
+  eat()
+  code()
+  coffee()
+  learn()
+  code()
+  never_give_up()
+  repeat()
+  
+def sleep():
+  return 'sleep well'
+  
+def eat():
+  return 'eat healthy'
+  
+  
+def code():
+  return 'maybe Python? or JavaScript?!?'
+  
+  
+def coffee():
+  return 'drink coffee if you feel sleepy or lazy'
+  
+def learn():
+  return 'learn everything actively'
+  
+def never_give_up():
+  return 'NEVER GIVE UP!!!'
+  
+def repeat():
+  return "just repeat all of the above without burnOut and any other kind of payco problem!"
+  
+life('Erkanat')
+
+t = [9,41,12,3,74,15]
+
+print(t[:4
+])
